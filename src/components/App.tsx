f359383d5f933d5eb39bf2315d0cc9604847e8dc
Dawid <david03h@wp.pl>
2019-10-31 02:10:38 +0800

    up

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import HelloWorld1 from './HelloWorld1/App';
-import HelloWorld2 from './HelloWorld2/App';
 
 export default class App extends React.Component<any,any>{
     constructor(props:any){
@@ -18,9 +17,6 @@ export default class App extends React.Component<any,any>{
                     <Switch>
                         <Route path="/helloworld1">
                             <HelloWorld1/>
-                        </Route>
-                        <Route path="/helloworld2">
-                            <HelloWorld2/>
                         </Route>
                     </Switch>
                 </div>
