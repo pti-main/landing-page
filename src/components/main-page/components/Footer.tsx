10667de7c2fb28e5922b1cdacf468479aa4acfd1
Filip Kania <fkania2@gmail.com>
2019-11-07 18:58:24 +0100

    0.0.2 => informations update

--- src/components/main-page/components/Footer.tsx
+++ src/components/main-page/components/Footer.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import config from '../../../../package.json';
+import { version } from '../../../../package.json';
 
 export default function Footer() {
     return(
         <div id="footer">
-            <span>asasas span czy sie cos zmieni czy nie bo nie wiem xddd</span>
-            <span className="build-version">{config.version}</span>
+            <span>{version}</span>
         </div>
     );
 }
